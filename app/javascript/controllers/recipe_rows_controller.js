5ee4ff53e071d8c46f4cadf26467d5705d5b590c
Christian Poplawski <christianpoplawski@gmail.com>
2020-04-30 23:21:36 +0200

    Styling of new recipe form

--- app/javascript/controllers/recipe_rows_controller.js
+++ app/javascript/controllers/recipe_rows_controller.js
@@ -18,18 +18,21 @@ export default class extends Controller {
 
     const wrapperDiv = document.createElement('div')
     wrapperDiv.setAttribute('data-target', 'recipe-rows.row')
-    wrapperDiv.innerHTML = `<div class="field mb-6">
-    <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_name">Name</label>
-    <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][ingredient_attributes][name]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_name">
-  </div>
-  <div class="field mb-6">
-    <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_amount">Amount</label>
-    <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][amount]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_amount">
-  </div>
-  <div class="field mb-6">
-    <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_unit">Unit</label>
-    <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][ingredient_attributes][unit]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_unit">
-  </div>`
+    wrapperDiv.setAttribute('class', 'flex flex-grow')
+    wrapperDiv.innerHTML = `
+      <div class="field mb-6 mr-3">
+        <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_name" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
+        <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][ingredient_attributes][name]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+      </div>
+      <div class="field mb-6 mr-3">
+        <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_amount" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Amount</label>
+        <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][amount]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_amount" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+      </div>
+      <div class="field mb-6">
+        <label for="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_unit" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Unit</label>
+        <input type="text" name="recipe[recipe_ingredients_attributes][${this.currentRowCount}][ingredient_attributes][unit]" id="recipe_recipe_ingredients_attributes_${this.currentRowCount}_ingredient_attributes_unit" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+      </div>
+  `
 
     this.containerTarget.appendChild(wrapperDiv)
   }
