34e3adba4c9e358b65d5b0db4f5a41aa5d8761ee
Christian Poplawski <christianpoplawski@gmail.com>
2020-12-09 12:31:42 +0100

    Add basic tests for shopping_list model

--- spec/models/shopping_list_spec.rb
+++ spec/models/shopping_list_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe ShoppingList, type: :model do
   describe "#build_from_meal_plan" do
     let(:meal_plan) { FactoryBot.create(:meal_plan) }
     let(:user) { FactoryBot.create(:user) }
+
     it "builds a valid shopping list" do
       shopping_list = ShoppingList.build_from_meal_plan(meal_plan, user)
-      expect(user.valid?).to be true
+      expect(shopping_list.valid?).to be true
+    end
+    
+    it "adds all the ingredients from the recipe to the shopping list" do
+      shopping_list = ShoppingList.build_from_meal_plan(meal_plan, user)
+      shopping_list.save
+      recipe_ingredients = meal_plan.recipes.map{ |rec| rec.recipe_ingredients }.flatten
+      expect(shopping_list.shopping_list_ingredients.count).to eql(recipe_ingredients.count)
     end
   end
 end
