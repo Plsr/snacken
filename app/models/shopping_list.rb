a3a8695caace76b5b364da0c2b10561b7a076cac
Christian Poplawski <christianpoplawski@gmail.com>
2020-12-09 23:51:18 +0100

    Refactore shopping list creation code

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -3,43 +3,9 @@ class ShoppingList < ApplicationRecord
   belongs_to :user
   has_many :shopping_list_ingredients
 
-  # TODO: Refactor in smaller chunks
   def self.build_from_meal_plan(meal_plan, user)
     recipe_ingredients = meal_plan.recipes.map { |rec| rec.recipe_ingredients }.flatten
-    
-    # Group ingredients by id
-    grouped_ingredients = recipe_ingredients.group_by{ |i| i.ingredient.id }
-    shopping_list_ingredients = []
-
-    # Loop over each value (i.e. each ingredient group)
-    grouped_ingredients.each_value do |ingredient_group|
-      ingredient = ingredient_group.first.ingredient
-
-      # Same ingredient is present more than once
-      if ingredient_group.length > 1 
-        # Group ingredients by unit
-        grouped_by_units = ingredient_group.group_by{ |i| i.unit }
-
-        # Placeholder to store amount strings in
-        amount = []
-
-        # Loop over every single unit group
-        grouped_by_units.each do |unit, ingredients| 
-            
-          # Sum up all the values for the current unit
-          total_amount = ingredients.map { |i| i.amount }.reduce(0, :+)
-
-          # return one string
-          amount << "#{total_amount} #{unit}"
-        end
-      # Same ingredient is only present once
-      else
-        amount = ["#{ingredient_group.first.amount} #{ingredient_group.first.unit}"]
-      end
-       
-
-      shopping_list_ingredients << { ingredient: ingredient, amount: amount }
-    end
+    shopping_list_ingredients = create_ingredients_from(recipe_ingredients)
 
     shopping_list = ShoppingList.new(
       user: user, 
@@ -48,5 +14,43 @@ class ShoppingList < ApplicationRecord
 
     shopping_list.shopping_list_ingredients.build(shopping_list_ingredients)
     shopping_list
+  end
+
+  private
+
+  # TODO: All of the code below should probably live in a service class
+  # at some point in the future (@plsr, 09/12/2020)
+  def self.amount_string_for(amount, unit)
+    "#{amount} #{unit}"
+  end
+
+  def self.create_ingredients_from(recipe_ingredients)
+    grouped_ingredients = recipe_ingredients.group_by{ |i| i.ingredient.id }
+    shopping_list_ingredients = []
+
+    grouped_ingredients.each_value do |ingredient_group|
+      ingredient = ingredient_group.first.ingredient
+      amount = amounts_for(ingredient_group)
+
+      shopping_list_ingredients << { ingredient: ingredient, amount: amount }
+    end
+
+    shopping_list_ingredients
+  end
+
+  def self.amounts_for(ingredient_group)
+    if ingredient_group.length > 1 
+      grouped_by_units = ingredient_group.group_by{ |i| i.unit }
+      amount = []
+
+      grouped_by_units.each do |unit, ingredients| 
+        total_amount = ingredients.map { |i| i.amount }.reduce(0, :+)
+        amount << amount_string_for(total_amount, unit)
+      end
+    else
+      amount = [amount_string_for(ingredient_group.first.amount, ingredient_group.first.unit)]
+    end
+
+    amount
   end
 end
