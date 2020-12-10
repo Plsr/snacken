bea706596cc80a67332120db69fd8098cf3cf197
Christian Poplawski <christianpoplawski@gmail.com>
2020-12-10 16:18:13 +0100

    Order shplst items by checked status
    Places the unchecked items on top and the unchecked items at the bottom

--- app/models/shopping_list_ingredient.rb
+++ app/models/shopping_list_ingredient.rb
@@ -5,6 +5,8 @@ class ShoppingListIngredient < ApplicationRecord
   validates_presence_of :ingredient
   validates_presence_of :amount
 
+  scope :checked_off_last, -> { order("checked_off_at DESC") }
+
   def ingredient_name
     ingredient.name
   end
