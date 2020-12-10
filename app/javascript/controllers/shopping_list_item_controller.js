ad1f2384b21071920e2be6847e3f0fbce2c43a02
Christian Poplawski <christianpoplawski@gmail.com>
2020-12-10 18:04:44 +0100

    Add raw version of dynamic sorting on check

--- app/javascript/controllers/shopping_list_item_controller.js
+++ app/javascript/controllers/shopping_list_item_controller.js
@@ -2,11 +2,38 @@ import { Controller } from "stimulus"
 import Rails from '@rails/ujs'
 
 export default class extends Controller {
+  static targets = ['itemList']
+
   toggle(event) {
-    const id = event.target.dataset.id
+    console.log(event)
+    event.preventDefault()
+    event.stopPropagation()
+    const id = event.currentTarget.dataset.id
+    const target = event.currentTarget
+
+    let checkbox
+    if (event.target instanceof HTMLInputElement) {
+      checkbox = event.target
+    } else {
+      checkbox = target.querySelector('input')
+    }
+
+    const wasCheckedOff = checkbox.checked
+
+
     Rails.ajax({
       type: "post",
       url: `/toggle_checked_off/${id}`,
     })
+
+
+
+    checkbox.checked = !checkbox.checked
+    this.itemListTarget.removeChild(target)
+    if (wasCheckedOff) {
+      this.itemListTarget.prepend(target)
+    } else {
+      this.itemListTarget.appendChild(target)
+    }
   }
 }
