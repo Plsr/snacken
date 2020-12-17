7dcb4c879225fe3736372b2802a2dd9aa748c8c9
Christian Poplawski <christianpoplawski@gmail.com>
2020-12-17 12:49:02 +0100

    Clean up JS

--- app/javascript/controllers/saved_recipe_row_controller.js
+++ app/javascript/controllers/saved_recipe_row_controller.js
@@ -1,12 +1,6 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["destroyToggle"];
-  
-  connect() {
-    console.log('connected')
-  }
-
   markForDelete(event) {
     const row = event.currentTarget.parentNode
     const input = row.querySelector("input[type='hidden']")
