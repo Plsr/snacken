8096c815a46aa1cee901077e352549c2e24e775a
Plsr <christianpoplawski@gmail.com>
2021-11-10 23:11:49 +0100

    Add delayed_job and send activation emails later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  before_action :ensure_activated
 
   private
+
+  def ensure_activated
+    return unless current_user
+    return if current_user.activated?
+    logout
+    redirect_to login_path
+  end
 
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
