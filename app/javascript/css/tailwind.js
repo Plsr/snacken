e2062bac2943a72f6fff80ca16e6367e095f4d10
Plsr <christianpoplawski@gmail.com>
2022-03-06 12:52:04 +0100

    I did a redesign before the initial design was finished

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,7 +1,8 @@
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ["Manrope"]
+      sans: ['Sarabun', 'sans-serif'],
+      serif: ['Roboto Slab', 'serif'],
     },
     extend: {}
   },
